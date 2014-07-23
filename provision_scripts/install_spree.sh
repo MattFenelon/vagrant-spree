c15119457bd46bfe39539b96a66bd20a2145f38f
Matthew Fenelon <matt@fnln.me>
2014-07-23 19:01:02 +0100

    Provision phantomjs and libmysqlclient-dev.
    Both are required by Spree.

--- provision_scripts/install_spree.sh
+++ provision_scripts/install_spree.sh
@@ -7,3 +7,13 @@ if [ $? == 0 ]; then
 else
     sudo apt-get install -y imagemagick
 fi
+
+# Install PhantomJS. A headless browser. Used in end-to-end testing.
+if which phantomjs > /dev/null; then
+  echo "Skipping PhantomJS installation as it's already installed."
+else
+  sudo apt-get install -y -q=1 phantomjs
+fi
+
+# Install SQLlite - required for new Rails sites
+sudo apt-get install -y -q=1 libmysqlclient-dev
